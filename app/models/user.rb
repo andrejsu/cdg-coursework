602357d78da9deef3a4e15e0a8de343954d1d9cb
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-03-06 00:42:41 +0300

    Added basic post resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+
   validates :nickname, presence: true, uniqueness: { case_sensitive: false }
   validates :name, length: { maximum: 50 }
   validates :bio, length: { maximum: 300 }
