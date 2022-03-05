ccaac5e5c498ea8f3a97f640d2fdc472f654ee11
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-03-06 00:38:58 +0300

    Fixed a resolution validation error

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'tailwindcss-rails', '~> 2.0'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
 
+gem 'fastimage'
 gem 'shrine', '~> 3.0'
 gem 'shrine-cloudinary', '~> 1.1'
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,18 +1,17 @@
 class ImageUploader < Shrine
-  ALLOWED_TYPES  = %w[image/jpeg image/png image/webp]
-  MAX_SIZE       = 10*1024*1024 # 10 MB
-  MAX_DIMENSIONS = [5000, 5000] # 5000x5000
+  ALLOWED_TYPES  = %w[image/jpeg image/png image/webp].freeze
+  MAX_SIZE       = 10 * 1024 * 1024 # 10 MB
+  MAX_DIMENSIONS = [5000, 5000].freeze # 5000x5000
 
   plugin :default_url
   plugin :validation_helpers
+  plugin :store_dimensions
 
   # File validations (requires `validation_helpers` plugin)
   Attacher.validate do
     validate_size 0..MAX_SIZE
 
-    if validate_mime_type ALLOWED_TYPES
-      validate_max_dimensions MAX_DIMENSIONS
-    end
+    validate_max_dimensions MAX_DIMENSIONS if validate_mime_type ALLOWED_TYPES
   end
 
   Attacher.default_url do |**options|
