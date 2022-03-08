9925715f896f883018a502220e7f414581e3cf00
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-03-08 22:07:00 +0300

    Decorated auth pages and some layout component

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
 
+  root to: 'static_pages#homepage'
+
   resources :users, only: [:show] do
     member do
       get :following, :followers
     end
   end
-  resources :follows, only: [:create]
-  resources :follows, only: [:destroy]
-
-  root to: 'public#homepage'
+  resources :follows, only: [:create, :destroy]
 
   resources :posts, only: [:index, :new, :create] do
     member do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Publics", type: :request do
+RSpec.describe "StaticPages", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
