f105a08afdf766598f65e73842aad6b6f75b9cac
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-03-09 11:34:37 +0300

    Disabled turbolink

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
@@ -12,6 +12,6 @@ import { themeChange } from "theme-change"
 
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 themeChange()
