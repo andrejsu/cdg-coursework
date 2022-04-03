e25d1782a0c2bf966e6636042124c5b5e809557f
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2022-04-04 00:36:08 +0300

    Improved the appearance and functionality of posts

--- app/controllers/feed_posts_controller.rb
+++ app/controllers/feed_posts_controller.rb
@@ -2,11 +2,10 @@ class FeedPostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.followings.exists?
-      @pagy, @posts = pagy Post.where(user_id: current_user.id).or(Post.where(user_id: current_user.followings.pluck(:id))).order(created_at: :desc)
-    else
-      @pagy, @posts = pagy Post.where(user_id: current_user.id)
-    end
+    @pagy, @posts = pagy Post.where(user_id: current_user.id)
+                             .or(Post.where(user_id: current_user.followings.pluck(:id)))
+                             .includes(:user, :likes)
+                             .order(created_at: :desc)
     @pagy_metadata = pagy_metadata(@pagy)
 
     respond_to do |format|
